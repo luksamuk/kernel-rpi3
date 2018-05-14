7baef2cd3b4860133d6a66ad65efb82aca4e9042
Lucas Vieira <lucasvieira@protonmail.com>
2018-05-13 23:07:12 -0300

    separated previous tests into their own functions

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 #include "rand.h"
 #include "delays.h"
 
+void test_mbox();
+void test_rng();
+void test_delay();
+
+
+
 void
 main()
 {
@@ -10,6 +16,24 @@ main()
     uart_init();
     rand_init();
 
+    test_mbox();
+
+    test_rng();
+
+    test_delay();
+
+    /* Echo everything back */
+    while(1) {
+	uart_send(uart_getc());
+    }
+}
+
+
+/* --- */
+
+void
+test_mbox()
+{
     /* Get board's unique serial number with a mailbox call */
     mbox[0] = 8 * 4;              /* Message length */
     mbox[1] = MBOX_REQUEST;       /* Flag as request */
@@ -31,12 +55,20 @@ main()
     } else {
 	uart_puts("Unable to query board serial number\n");
     }
+}
 
+void
+test_rng()
+{
     /* test rng */
     uart_puts("Here, have a random number: ");
     uart_hex(rand(0, 4294967295));
     uart_puts("\n");
+}
 
+void
+test_delay()
+{
     uart_puts("Waiting 10000000 CPU cycles (ARM CPU): ");
     wait_cycles(10000000);
     uart_puts("OK\n");
@@ -51,10 +83,5 @@ main()
     } else {
 	wait_msec_st(1000000);
 	uart_puts("OK\n");
-    }
-
-    /* Echo everything back */
-    while(1) {
-	uart_send(uart_getc());
     }
 }
